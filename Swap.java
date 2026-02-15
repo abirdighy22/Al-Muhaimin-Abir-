918f5e6b488da8b80cbde1fbc0e5b8ea9575bfc9
Al Muhaimin Abir <abirdakbd@gmail.com>
2026-02-15 23:09:59 +0600

    Update Swap.java

--- Swap.java
+++ Swap.java
@@ -1,8 +1,11 @@
+import java.util.Scanner; 
 public class Swap {
     public static void main(String[] args) {
-    
-        int a=7;
-        int b=5;
+            Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a=sc.nextInt();
+        System.out.print("Enter b: ");
+        int b=sc.nextInt();
 
         a=a+b;
         b=a-b;
