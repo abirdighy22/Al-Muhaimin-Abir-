9284f7fbf84e6dd2572451eaf160cc65a4748b88
Al Muhaimin Abir <abirdakbd@gmail.com>
2026-02-22 22:01:44 +0600

    Update Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -1,18 +1,20 @@
-public class Pyramid {
+import java.util.Scanner;
+
+public class StarPyramid {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
 
-        int a = 5;
-
-        for (int d = 1; d <= a; d++) {
-
-            for (int j = 1; j <= a - d; j++) {
+        for (int i = 1; i <= rows; i++) {
+            // Print spaces for symmetry
+            for (int j = 1; j <= rows - i; j++) {
                 System.out.print(" ");
             }
-
-            for (int k = 1; k <= 2 * d - 1; k++) {
+            // Print stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print("*");
             }
-
             System.out.println();
         }
     }
