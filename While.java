9af340dc9c18a5e80395477cbe886af75b0b756d
Al Muhaimin Abir <abirdakbd@gmail.com>
2026-02-22 20:16:53 +0600

    Create While.java

---
+++ While.java
@@ -0,0 +1,11 @@
+public class While {
+    public static void main(String[] args) {
+
+        int d = 1;
+
+        while (d <= 5) {
+            System.out.println(d);
+            d++;
+        }
+    }
+}
